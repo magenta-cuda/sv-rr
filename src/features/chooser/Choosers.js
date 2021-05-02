82020055e0d1143142974e7661c0daf43c5ec0c3
Soft DuckTape <softducktape@gmail.com>
2021-05-02 08:20:07 -1000

    clean Choosers.js

--- src/features/chooser/Choosers.js
+++ src/features/chooser/Choosers.js
@@ -3,10 +3,10 @@ import { Table } from '../table/Table';
 import styles from './Choosers.module.css';
 
 export function Choosers(props) {
-    let cells = []
-    props.cells.forEach(function(item, index, array) {
+    const cells = props.cells.reduce((cells, item) => {
         cells.push(<Chooser name={item.name} selected={item.selected} cells={item.options} />)
-    })
+        return cells
+    }, [])
     return (
         <div className={styles.div}>
             <Table ncols={2} cells={cells} />
