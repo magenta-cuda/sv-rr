140b9f9c0fbda1328b8091018612f19bf2cfb521
Soft DuckTape <softducktape@gmail.com>
2021-05-02 09:00:59 -1000

    clean Chooser.js

--- src/features/chooser/Chooser.js
+++ src/features/chooser/Chooser.js
@@ -1,14 +1,14 @@
-import { useDispatch } from 'react-redux';
-import { Table } from '../table/Table';
-import { setSelected } from './choosersSlice';
-import styles from './Chooser.module.css';
+import { useDispatch } from 'react-redux'
+import { Table } from '../table/Table'
+import { setSelected } from './choosersSlice'
+import styles from './Chooser.module.css'
 
 export function Chooser(props) {
     const dispatch = useDispatch()
-    let cells = []
-    props.cells.forEach(function(item, index, array) {
+    const cells    = props.cells.reduce((cells, item) => {
         cells.push(<img className={styles.thumbnail} src={item.image} alt={item.selection} onClick={() => dispatch(setSelected(item.id))} />)
-    })
+        return cells
+    }, [])
     return (
         <div className={styles.div}>
             <img className={styles.img} src={props.selected?props.selected.fullsize:''} alt={props.name} />
