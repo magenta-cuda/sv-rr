764b8a934feae3bc1606be056071c53e4a03d83c
Soft DuckTape <softducktape@gmail.com>
2021-05-02 23:38:28 -1000

    add titles to images

--- src/features/chooser/Chooser.js
+++ src/features/chooser/Chooser.js
@@ -1,17 +1,21 @@
 import { useDispatch } from 'react-redux'
 import { Table } from '../table/Table'
 import { setSelected } from './choosersSlice'
+import { currency } from '../../app/globals'
 import styles from './Chooser.module.css'
 
 export function Chooser(props) {
     const dispatch = useDispatch()
     const cells    = props.cells.reduce((cells, item) => {
-        cells.push(<img className={styles.thumbnail} src={item.image} alt={item.selection} onClick={() => dispatch(setSelected(item.id))} />)
+        cells.push(<img className={styles.thumbnail} src={item.image} alt={item.selection}
+                        title={`${item.selection}: ${currency}${Number(item.price).toFixed(2)}`}
+                        onClick={() => dispatch(setSelected(item.id))} />)
         return cells
     }, [])
     return (
         <div className={styles.div}>
-            <img className={styles.img} src={props.selected?props.selected.fullsize:''} alt={props.name} />
+            <img className={styles.img} src={props.selected?props.selected.fullsize:''} alt={props.name}
+                 title={`${props.selected.selection}: ${currency}${Number(props.selected.price).toFixed(2)}`} />
             <Table ncols={3} cells={cells} />
         </div>
     )
