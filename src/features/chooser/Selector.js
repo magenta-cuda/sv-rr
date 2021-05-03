a158dc3921a4ee9693f003873b7f608abd642bc0
Soft DuckTape <softducktape@gmail.com>
2021-05-03 10:57:24 -1000

    add price to options in select

--- src/features/chooser/Selector.js
+++ src/features/chooser/Selector.js
@@ -1,14 +1,22 @@
-import { useDispatch } from 'react-redux';
-import { setSelected } from './choosersSlice';
+import { useDispatch } from 'react-redux'
+import { setSelected } from './choosersSlice'
 import { currency } from '../../app/globals'
-import styles from './Selector.module.css';
+import styles from './Selector.module.css'
 
 export function Selector(props) {
     const dispatch = useDispatch()
     return (
         <>
             <select className={styles.select} onChange={(e) => dispatch(setSelected(e.target.value))}>
-                {props.options.map(option => <option value={option.id} selected={option===props.selected}>{option.selection}</option>)}
+                {props.options.map(option => {
+                    let price = Number(option.price)
+                    price     = price ? `: ${currency}${price.toFixed(2)}` : ''
+                    return (
+                        <option value={option.id} selected={option===props.selected}>
+                            {`${option.selection}${price}`}
+                        </option>
+                    )
+                })}
             </select>
             <div className={styles.descriptionDiv}>{props.selected.description}</div>
             <div className={styles.priceDiv}>{`Price: ${currency}${Number(props.selected.price).toFixed(2)}`}</div>
