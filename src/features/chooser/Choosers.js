2d15ee14379ae7c5e2f5319d997d1889c30c3b2c
Soft DuckTape <softducktape@gmail.com>
2021-05-15 19:59:10 -1000

    call wc_product_gallery()

--- src/features/chooser/Choosers.js
+++ src/features/chooser/Choosers.js
@@ -1,3 +1,4 @@
+import {useEffect} from 'react'
 import {Chooser} from './Chooser'
 import {Table} from '../table/Table'
 import {chooserClass} from '../../app/globals'
@@ -8,6 +9,17 @@ export function Choosers(props) {
         cells.push(<Chooser chooserClass={chooserClass} name={item.name} selected={item.selected} cells={item.options} />)
         return cells
     }, [])
+    useEffect(() => {
+        if (window.hasOwnProperty('jQuery') && window.jQuery.hasOwnProperty('fn') && window.jQuery.fn.hasOwnProperty('wc_product_gallery')) {
+            window.jQuery('.woocommerce-product-gallery').each(function() {
+                const $this = window.jQuery(this)
+                console.log('$this.data("product_gallery")', $this.data('product_gallery'))
+                if ($this.data('product_gallery') === undefined) {
+                    $this.wc_product_gallery()
+                }
+            })
+        }
+    })
     return (
         <div className={styles.div}>
             <Table tableClass='' cellClass='' ncols={2} cells={cells} />
