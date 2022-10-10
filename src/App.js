6620369ec86ce3be6098518b2ae449997af3947a
Omega Turtle <msxl97@yahoo.com>
2022-10-09 20:20:27 -1000

    better not found page

--- src/App.js
+++ src/App.js
@@ -3,18 +3,24 @@ import { useSelector } from 'react-redux';
 import { Choosers } from './features/chooser/Choosers';
 import { Selectors } from './features/chooser/Selectors';
 import { Orderer } from './features/orderer/Orderer'
-import { selectData, selectProductName } from './features/chooser/choosersSlice';
+import { selectData, selectProductName, selectProductId } from './features/chooser/choosersSlice';
 import './App.css';
 
 function App() {
     const data        = useSelector(selectData)
     const productName = useSelector(selectProductName)
+    const productId   = useSelector(selectProductId)
     return (
         <div className="SV-Redux-App">
             {typeof data === "string"
                 ? <div className="SV-Redux-App-productName">{data}</div>
                 : !data.length
-                    ? <div className="SV-Redux-App-productName">Product does not exists.</div>
+                    ? <>
+                        <div className="SV-Redux-App-productName">Simple Variations</div>
+                        <div className="SV-Redux-App-message">
+                            <span>SV Product {productId} not found.</span>
+                        </div>
+                    </>
                     : <>
                         <div className="SV-Redux-App-productName">{productName}</div>
                         <header className="SV-Redux-App-header">
